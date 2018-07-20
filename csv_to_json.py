e2f1b7e3e5a80f7d9cc0b03dcd58a6009086a02f
Josiah Kiehl <josiah@capoferro.net>
2018-07-19 20:26:24 -0400

    German (and in theory, other language) support

--- csv_to_json.py
+++ csv_to_json.py
@@ -15,41 +15,51 @@ set_prefixes = {
 
 int_fields = [
     "glory",
-    "id"
+    "id",
+    "number"
 ]
 
+EN = "en"
+DE = "de"
+supported_locales = [EN, DE]
+
 def main():
-    gw_data = fetch_gw_data()
+    for locale in supported_locales:
+        process_locale(locale)
+
+def process_locale(locale):
+    gw_data = fetch_gw_data(locale)
     cards = gw_to_cards(gw_data)
     gw_name_map = {}
+    gw_number_map = {}
     for c in cards:
         gw_name_map[c["name"]] = c
+        number = int(c["gw_number"])
+        gw_number_map[number] = c
 
-    csv_data = read_csv('cards-en_UK.csv')
+    csv_data = read_csv('cards-{}.csv'.format(locale))
     csv_name_map = {}
     for c in csv_data:
         name = c["name"]
         if name in gw_name_inaccuracies:
             name = gw_name_inaccuracies[name]
         if name not in gw_name_map:
-            print("{} was not found in GW data!".format(name))
+            correct_name = gw_number_map[c["number"]]["name"]
+            print("Name mismatch (#{}): {} -> {}".format(c["number"], name, correct_name))
+            c["name"] = correct_name
+            name = correct_name
         hydrate_card_with_gw_data(c, gw_name_map[name])
-        for field in int_fields:
-            try:
-                c[field] = int(c[field])
-            except:
-                c[field] = 0
+        intify(c)
         csv_name_map[name] = c
-    
-    with open('cards-Missing.csv', 'w+') as missing_cards_csvfile:
+
+    with open('cards-Missing-{}.csv'.format(locale), 'w+') as missing_cards_csvfile:
         writer = csv.DictWriter(missing_cards_csvfile, fieldnames=cards[0].keys())
         writer.writeheader()
         for c in cards:
             if c["name"] not in csv_name_map:
-                print("{} was not found in our data!".format(c["name"]))
                 writer.writerow(c)
 
-    with open('cards-en_UK.json', 'w+') as jsonfile:
+    with open('cards-{}.json'.format(locale), 'w+') as jsonfile:
         json.dump(list(csv_name_map.values()), jsonfile, sort_keys=True, indent=2)
 
     for c in csv_name_map.values():
@@ -62,8 +72,13 @@ def main():
             with open(filepath, 'wb') as imgfile:
                 imgfile.write(response.content)
 
-
-    
+def intify(c):
+    for field in int_fields:
+        try:
+            c[field] = int(c[field])
+        except:
+            c[field] = 0
+    return c
 
 def hydrate_card_with_gw_data(card, gw):
     for key, value in gw.items():
@@ -78,8 +93,13 @@ def hydrate_card_with_gw_data(card, gw):
     if card["gw_card_set_id"] in set_prefixes:
         card["set_prefix"] = set_prefixes[card["gw_card_set_id"]]
 
-def fetch_gw_data():
-    response = requests.get("https://warhammerunderworlds.com/wp-json/wp/v2/cards/?ver=13&per_page=1000")
+def fetch_gw_data(locale):
+    locale_query = ""
+    if locale != "en":
+        locale_query = "&lang=" + locale
+    url = "https://warhammerunderworlds.com/wp-json/wp/v2/cards/?ver=13&per_page=1000" + locale_query
+    print(url)
+    response = requests.get(url)
     if response.status_code != 200:
         print("Error ({}) fetching GW data".format(response.status_code))
         return None
@@ -90,8 +110,7 @@ def gw_to_cards(gw_data):
     return [create_card_from_gw(gw) for gw in gw_data]
 
 # some names in GW data are '123. Foo' and others are just 'Foo'
-def normalize_name(name):
-    
+def normalize_name(name):   
     if '.' in name:
         name = '.'.join(name.split('.')[1:]).strip()
     return name.replace(u"\u2018", "'").replace(u"\u2019", "'").replace("&#8217;", "'").replace("&#8216;", "'")
@@ -112,6 +131,6 @@ def create_card_from_gw(gw):
 def read_csv(csvpath):
     with open(csvpath, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
-        return [card for card in reader]
+        return [intify(card) for card in reader]
 
 main()
